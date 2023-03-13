f71b769abe00ab362769dcf8378d8c2d86809631
achmadrachmandika <fourhope666@gmail.com>
2023-03-13 20:20:27 +0700

    first commit

---
+++ 03_praktikum_web_lanjut_dua/resources/views/footer.blade.php
@@ -0,0 +1,7 @@
+
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> 3.2.0
+    </div>
+    <strong>Copyright &copy; 2023 <a href="https://adminlte.io">Achmad Rachmandika Rizky Pratama</a>.</strong> All rights reserved.
+</footer>
